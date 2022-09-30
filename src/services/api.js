e7312812b86d64fe4c375e06437021e33ab2780c
Hessam Imen <hessam.imen@gmail.com>
2022-10-01 09:37:51 +1000

    initial commit

---
+++ src/services/api.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import axios from 'axios'
+
+const BASE_URL = "https://fakestoreapi.com"
+
+function api() {
+  return (
+    <div>api</div>
+  )
+}
+
+export default api
