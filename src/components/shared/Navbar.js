2c2cf060ad98831a0aa9c182020d4fba9a977f42
Hessam Imen <hessam.imen@gmail.com>
2022-10-24 10:43:19 +1100

    Basis design for Home Page is added

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -33,14 +33,14 @@ const Navbar = () => {
                         <Link to="/signup" className='cursor-default hover:bg-gray-200 rounded-xl p-2 m-2'>Sign Up</Link>
                     </div>
                     <div className='sm:hidden mr-4' onClick={handleClick}>
-                        {!nav ? <Bars3Icon className='w-5'/> : <XMarkIcon className='w-5'/>}
+                        {!nav ? <Bars3Icon className='w-7'/> : <XMarkIcon className='w-7'/>}
                     </div>
                     {/* test */}
                     <ul className={!nav ? 'hidden' : 'absolute top-16 left-0 bg-black text-white w-full sm:hidden'}>
-                        <li className='border-b-2 border-zinc-300 cursor-default hover:bg-gray-200 h-10 text-center p-2'><Link to="/" onClick={handleClick}>Home</Link></li>
-                        <li className='border-b-2 border-zinc-300  cursor-default hover:bg-gray-200 h-10 text-center p-2'><Link to="/products" onClick={handleClick} >Products</Link></li>
-                        <li className='border-b-2 border-zinc-300  cursor-default hover:bg-gray-200 h-10 text-center p-2'><Link to="/aboutus" onClick={handleClick}>About Us</Link></li>
-                        <li className='border-b-2 border-zinc-300  cursor-default hover:bg-gray-200 h-10 text-center p-2'>  <Link to="/signup" onClick={handleClick}>Sign Up</Link></li>
+                        <li className='border-b-2 border-zinc-300 cursor-default hover:bg-slate-600/80 h-10 text-center p-2'><Link to="/" onClick={handleClick}>Home</Link></li>
+                        <li className='border-b-2 border-zinc-300  cursor-default hover:bg-slate-600/80 h-10 text-center p-2'><Link to="/products" onClick={handleClick} >Products</Link></li>
+                        <li className='border-b-2 border-zinc-300  cursor-default hover:bg-slate-600/80 h-10 text-center p-2'><Link to="/aboutus" onClick={handleClick}>About Us</Link></li>
+                        <li className='border-b-2 border-zinc-300  cursor-default hover:bg-slate-600/80 h-10 text-center p-2'>  <Link to="/signup" onClick={handleClick}>Sign Up</Link></li>
                     </ul>
                 </div>
                 <div className='relative mr-8'>
